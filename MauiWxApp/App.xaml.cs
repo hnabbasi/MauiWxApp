fb731c0b54070ba3337531419ad4b014638ba4cf
Hussain Abbasi <hnabbasi@outlook.com>
2022-09-10 22:42:47 -0500

    Intial app

---
+++ MauiWxApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiWxApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiWxApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiWxApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
+
